88f0dd44d86f682f6be34a49a698576a42e4d310
Zachery Cox <zacherycox@zacherys.macbook.pro.home>
2026-06-11 22:51:27 -0400

    Testing

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -6,11 +6,11 @@ output "name" {
   value = "test"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "b3691909d8800f0e36eeff26aa6e497412"
+# resource "aws_s3_bucket" "example" {
+#   bucket = "b3691909d8800f0e36eeff26aa6e497412"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "TEST"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "TEST"
+#   }
+# }

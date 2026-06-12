190a19fbfe12a1e91db51b5b128d399a4bc2e459
Zachery Cox <zacherycox@zacherys.macbook.pro.home>
2026-06-11 22:32:56 -0400

    Testing

---
+++ kubernetes/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+output "name" {
+  value = "test"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "b3691909d8800f0e36eeff26aa6e49741"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "TEST"
+  }
+}
